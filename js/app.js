25e1e9a75755707e300e623cbac5181e2630f126
Alberto Francesco Motta <alberto.motta.86@gmail.com>
2015-12-12 14:04:28 +0100

    feat: Cat model separeted from ViewModel

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
-var ViewModel = function () {
+var Cat = function (){
   this.name = ko.observable('Pino');
   this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
   this.clickCount = ko.observable(0);
-  this.imgAuthor = ko.observable();
+  this.imgAuthor = ko.observable('ciao');
+  this.nicknames = ko.observableArray(['Bert', 'Pinuccio', 'Gatto', 'Felino']);
   this.level = ko.computed(function(){
     if (this.clickCount() <= 10) {return 'infant';}
     else if (10 < this.clickCount() && this.clickCount() <= 20) {return 'young';}
     else if (this.clickCount() > 20){return 'adult';}
   }, this);
+};
 
+var ViewModel = function () {
+  this.currentCat = ko.observable(new Cat());
   this.incrementCounter = function(){
-    this.clickCount(this.clickCount() + 1);
+    this.currentCat().clickCount(this.currentCat().clickCount() + 1);
   };
 };
 
